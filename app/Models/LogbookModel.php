e0a75939fa87c75a9d561f39a07fc9f9848244b8
hsf-apple <muhd.hasifx9@gmail.com>
2022-01-15 21:52:14 +0800

    modle logbook

---
+++ app/Models/LogbookModel.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class LogbookModel extends Model
+{
+    use HasFactory;
+
+    protected $table = 'studentprofile';
+
+    protected $fillable= [
+        'meetingDate',
+        'startTime',
+        'endTime',
+        'currentProgress',
+        'discDetail',
+        'actPlan'
+    ];
+
+    
+}
